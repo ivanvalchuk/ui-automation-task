c6aec8dba9d40df79812e16267ac41a96a4bd470
Ivan Valchuk <ivan.valchuk@gmail.com>
2025-03-19 12:53:25 +0100

    Updating 1st test

--- test_task.py
+++ test_task.py
@@ -1,10 +1,17 @@
-import re
+import pytest
 from playwright.sync_api import Page, expect
 
+@pytest.fixture(scope="function", autouse=True)
+def before_each_after_each (page: Page):
+        
+        # Go to the starting url before each test.
+        page.goto("https://www.bamfunds.com/")
+
+        # Accept the use of cookies
+        page.get_by_role("button", name="Accept cookies").click()
 
 def test_founders_section(page: Page):
-    page.goto("https://www.bamfunds.com/")
-    page.get_by_role("button", name="Accept cookies").click()
+
     page.get_by_label("Header").get_by_role("link", name="About us").hover()
     page.get_by_label("Header").get_by_role("link", name="Leadership").click()
     expect(page.get_by_role("link", name="Leadership Headshot Dmitry")).to_be_visible()
