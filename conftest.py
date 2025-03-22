fe42570838beac8eb74b24fa2f0f3db679a1fb7a
Ivan Valchuk <ivan.valchuk@gmail.com>
2025-03-22 21:24:57 +0100

    Additonal changes

--- conftest.py
+++ conftest.py
@@ -9,48 +9,53 @@ def get_playwright():
     with sync_playwright() as playwright:
         yield playwright
 
-# @fixture
-# def get_browser(get_playwright, request):
-#     browser = request.config.getoption('--browser')
-#     headless = request.config.getini('headless')
+@fixture
+def get_browser(get_playwright, request):
+    browser = ' '.join(request.config.getoption('--browser'))
+    headless = request.config.getini('headless')
     
-#     if headless == 'True':
-#         headless = True
-#     else:
-#         headless = False
-
-#     if browser == 'chromium':
-#         bro = get_playwright.chromium.launch(headless = headless)
-#     elif browser == 'firefox':
-#         bro = get_playwright.firefox.launch(headless = headless)
-#     elif browser == 'webkit':
-#         bro = get_playwright.webkit.launch(headless = headless)
-#     else:
-#         assert False, 'unsupported browser type'
+    if headless == 'True':
+        headless = True
+    else:
+        headless = False
+    if browser == 'chromium':
+        bro = get_playwright.chromium.launch(headless = headless)
+    elif browser == 'firefox':
+        bro = get_playwright.firefox.launch(headless = headless)
+    elif browser == 'webkit':
+        bro = get_playwright.webkit.launch(headless = headless)
+    else:
+        assert False, 'unsupported browser type'
     
-#     yield bro
-#     bro.close()
+    yield bro
+    bro.close()
 
 @fixture
-def desktop_app(get_playwright, request):
+def desktop_app(get_browser, request):
     base_url = request.config.getini('base_url')
-    app = App(get_playwright, base_url=base_url)
+    app = App(get_browser, base_url=base_url)
     app.goto('/')
     yield app
     app.close()
 
 @fixture
-def mobile_app(get_playwright, request):
+def mobile_app(get_playwright, get_browser, request):
     base_url = request.config.getini('base_url')
     device = request.config.getoption('--device')
-    app = App(get_playwright, base_url= base_url, device= device)
+    device_config = get_playwright.devices.get(device)
+
+    if device_config is not None:
+        device_config.update(**device_config)
+    else:
+        device_config = device
+
+    app = App(get_browser, base_url= base_url, **device_config)
     app.goto('/')
     yield app
     app.close()
 
 def pytest_addoption(parser):
 #     # parser.addoption('--device', action ='store', default= '')
-#     # parser.addoption('--browser', action ='store', default= 'chromium')
+    # parser.addoption('--browser', action ='store', default= 'chromium')
 #     # parser.addini('base_url', help = "base url of the site under test", default = "https://www.bamfunds.com")
     parser.addini('headless', help = "run browser in headless mode")
-
--- page_objects/application.py
+++ page_objects/application.py
@@ -1,17 +1,10 @@
-from playwright.sync_api import Playwright
+from playwright.sync_api import Browser
 from .test_cases import TestCases
 
 class App:
-    def __init__(self, playwright: Playwright, base_url: str, headless=False, device=None, **kwargs):
-        device_config = playwright.devices.get(device)
-
-        if device_config is not None:
-            device_config.update(kwargs)
-        else:
-            device_config = kwargs
-
-        self.browser = playwright.chromium.launch(headless = headless)
-        self.context = self.browser.new_context(**device_config)
+    def __init__(self, browser: Browser, base_url: str, **kwargs):
+        self.browser = browser
+        self.context = self.browser.new_context(**kwargs)
         self.page = self.context.new_page()
         self.base_url = base_url
         self.test_cases = TestCases(self.page)
@@ -109,4 +102,3 @@ class App:
     def close(self):
         self.page.close()
         self.context.close()
-        self.browser.close()
