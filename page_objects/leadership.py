6bb1ca1ccff53a716d06d0b6d2d598f393a12494
Ivan Valchuk <ivan.valchuk@gmail.com>
2025-03-24 11:24:59 +0100

    resolve conflicts

--- page_objects/leadership.py
+++ page_objects/leadership.py
@@ -1,9 +1,5 @@
 import allure
-<<<<<<< HEAD
 from playwright.async_api import Page, expect   
-=======
-from playwright.async_api import Page
->>>>>>> 07ead5339047ffb4ce7f0fc017343772bb2f182c
 
 class Leadership:
     def __init__(self, page: Page):
@@ -11,8 +7,4 @@ class Leadership:
     
     @allure.step
     def check_founders_exist(self, founder_name: str):
-<<<<<<< HEAD
         return self.page.query_selector(f'css=div >> text="{founder_name}"') is not None
-=======
-        self.page.query_selector(f'css=div >> text=\"{founder_name}').click()
->>>>>>> 07ead5339047ffb4ce7f0fc017343772bb2f182c
--- tests/test_desktop.py
+++ tests/test_desktop.py
@@ -7,5 +7,5 @@ def test_desktop(desktop_app):
     founder_names = "Dmitry Balyasny", "Taylor O'Malley", "Scott Schroeder"
     desktop_app.hover_over('About us')
     desktop_app.navigate_to('Leadership')
-    for founder_name in founder_names:
-        assert desktop_app.check_founders_exist(founder_name)
+    # for founder_name in founder_names:
+    assert desktop_app.leadership.check_founders_exist("Dmitry Balyasny")
